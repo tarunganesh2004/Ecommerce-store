3a0c6adc020b39ee3f7b0039792b7a861f229231
tarunganesh2004 <tarun.21bce8974@vitapstudent.ac.in>
2025-02-17 19:57:15 +0530

    Update database connection configuration to use environment variables

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,14 @@
 import mysql from "mysql2"
+import dotenv from "dotenv"
 
+dotenv.config();
 
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'nagasai@9',
-    database: 'tg'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
 });
 
 export const connectDB = () => {
