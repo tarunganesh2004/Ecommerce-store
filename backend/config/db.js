f1ad81cf5f525e4dae2e42b28fe5c321ff0dad78
tarunganesh2004 <tarun.21bce8974@vitapstudent.ac.in>
2025-02-17 19:53:42 +0530

    Update database configuration and environment variables

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,12 @@
 import mysql from "mysql2"
 
+
+
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+    host: 'localhost',
+    user: 'root',
+    password: 'nagasai@9',
+    database: 'tg'
 });
 
 export const connectDB = () => {
